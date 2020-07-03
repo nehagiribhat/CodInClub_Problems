0e2d501cfa3356df76c325df46b6d0b08ec4d0ed
nehagiribhat <nehagiribhat@gmail.com>
2020-07-03 11:01:10 +0530

    Part Time Employee Pay

--- empWageProblem.sh
+++ empWageProblem.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -x
 #constants
 WAGE_PER_HR=20
+FULL_TIME=2
+PART_TIME=1
 
 attendance=$((RANDOM%3))
 
-if [[ $attendance -eq 2 ]]
+if [[ $attendance -eq $FULL_TIME ]]
+then
+	workHour=10
+elif [[ $attendance -eq $PART_TIME ]]
 then
 	workHour=8
 else
