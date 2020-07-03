237037bfb2aa62956472e6e9209773ca122748d0
nehagiribhat <nehagiribhat@gmail.com>
2020-07-03 12:09:43 +0530

    Calculating Wages For a Condition

--- empWageProblem.sh
+++ empWageProblem.sh
@@ -4,14 +4,24 @@ WAGE_PER_HR=20
 FULL_TIME=2
 PART_TIME=1
 WORKING_DAYS_PER_MONTH=20
+WORKING_HRS_PER_MONTH=100
 
-attendance=$((RANDOM%3))
+#variables
+total_working_days=0
+total_working_hrs=0
 
-case $attendance in
-	$FULL_TIME) workHour=10;;
-	$PART_TIME) workHour=8;;
-	*) workHour=0;;
-esac
-emp_wage_perday=$(($WAGE_PER_HR*$workHour))
-emp_wage_permonth=$(($emp_wage_perday*$WORKING_DAYS_PER_MONTH))
+while [ $total_working_days -le $WORKING_DAYS_PER_MONTH ] && [ $total_working_hrs -le $WORKING_HRS_PER_MONTH ]
+do
+	((total_working_days++))
+	attendance=$((RANDOM%3))
+
+	case $attendance in
+		$FULL_TIME) workHour=10;;
+		$PART_TIME) workHour=8;;
+		*) workHour=0;;
+	esac
+	total_working_hrs=$(($total_working_hrs+$workHour))
+done
+
+emp_wage_permonth=$(($total_working_hrs*$WAGE_PER_HR))
 echo "Employees per month pay is $emp_wage_permonth"
