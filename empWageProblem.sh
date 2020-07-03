f075dce81d433f0ddd4cb0bf766ac35af9872043
nehagiribhat <nehagiribhat@gmail.com>
2020-07-03 12:28:42 +0530

    Employee Wage Using Function

--- empWageProblem.sh
+++ empWageProblem.sh
@@ -10,17 +10,21 @@ WORKING_HRS_PER_MONTH=100
 total_working_days=0
 total_working_hrs=0
 
+#function
+function getWorkingHrs(){
+ 	case $1 in
+                $FULL_TIME) workHour=10;;
+                $PART_TIME) workHour=8;;
+                *) workHour=0;;
+	esac
+	echo $workHour
+}
+
 while [ $total_working_days -le $WORKING_DAYS_PER_MONTH ] && [ $total_working_hrs -le $WORKING_HRS_PER_MONTH ]
 do
 	((total_working_days++))
-	attendance=$((RANDOM%3))
-
-	case $attendance in
-		$FULL_TIME) workHour=10;;
-		$PART_TIME) workHour=8;;
-		*) workHour=0;;
-	esac
-	total_working_hrs=$(($total_working_hrs+$workHour))
+	workingHrs=$( getWorkingHrs $((RANDOM%3)) )
+	total_working_hrs=$(($total_working_hrs+$workingHrs))
 done
 
 emp_wage_permonth=$(($total_working_hrs*$WAGE_PER_HR))
