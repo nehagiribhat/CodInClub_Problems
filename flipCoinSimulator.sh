efe487420d9596771130ba6047ea3e6620a10475
nehagiribhat <nehagiribhat@gmail.com>
2020-07-03 17:51:00 +0530

    Getting Difference of Wins for 21 Flips

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -5,18 +5,32 @@ flip=0
 total_no_of_heads=0
 total_no_of_tails=0
 
-read -p "Enter number of times to flip: " limit
-while [ $flip -lt $limit ]
+while [ $flip -lt 21 ]
 do
 	outcome=$((RANDOM%2))
 	if [ $outcome -eq 1 ]
 	then
-		((total_no_of_heads++))
+		((flip++))
+		total_no_of_heads=$flip
 	else
-		((total_no_of_tails++))
+		((flip++))
+		total_no_of_tails=$flip
 	fi
-	((flip++))
 done
 
 echo "Heads occured $total_no_of_heads times"
 echo "Tails occured $total_no_of_tails times"
+
+if [ $total_no_of_heads -eq $total_no_of_tails ]
+then
+	echo "It's a TIE"
+else
+	if [ $total_no_of_heads -eq $total_no_of_tails ]
+	then
+		difference=$((total_no_of_heads-total_no_of_tails))
+		echo "Heads Wins by $difference"
+	else
+		difference=$((total_no_of_tails-total_no_of_heads))
+                echo "Tails Wins by $difference"
+	fi
+fi
