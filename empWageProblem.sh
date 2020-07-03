7ccd116f59b16b9f897b7f2a45cc703a6099d713
nehagiribhat <nehagiribhat@gmail.com>
2020-07-03 11:45:38 +0530

    Employee Per Day Pay Using Switch Case

--- empWageProblem.sh
+++ empWageProblem.sh
@@ -6,14 +6,10 @@ PART_TIME=1
 
 attendance=$((RANDOM%3))
 
-if [[ $attendance -eq $FULL_TIME ]]
-then
-	workHour=10
-elif [[ $attendance -eq $PART_TIME ]]
-then
-	workHour=8
-else
-	workHour=0
-fi
+case $attendance in
+	$FULL_TIME) workHour=10;;
+	$PART_TIME) workHour=8;;
+	*) workHour=0;;
+esac
 emp_wage_perday=$(($WAGE_PER_HR*$workHour))
 echo "Employees per day pay is $emp_wage_perday"
